85f3198e561223cb5f8a736c91a553f790e9985c
sonal <sonalichaudhari219@gmail.com>
2025-01-24 20:44:14 +0530

    new commit

--- src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -1,8 +1,6 @@
 package net.engineeringdigest.journalApp.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -13,7 +11,9 @@ import java.util.List;
 
 @Document(collection = "users")
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -21,6 +21,8 @@ public class User {
     @Indexed(unique = true)
     @NonNull
     private String userName;
+    private String email;
+    private boolean sentimentAnalysis;
     @NonNull
     private String password;
     @DBRef
